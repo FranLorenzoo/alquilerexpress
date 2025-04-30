538f0a9ada8065a37c59924a5181e78cf49229c6
Gasparsanchez334 <gasparsanchez334@gmail.com>
2025-04-30 16:06:39 -0300

    nuevo

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,20 @@ export default function Pruebas (){
         <Head>
             <title> Alquileres</title>
         </Head>
-        <main className="flex justify-center w-screen h-screen bg-black">
-            <div className="flex flex-col w-1/2 h-1/3 py-10 bg-black ">      
-            <CuadradoAlquiler className="w-1/2"nom="informatica" ciu="la plata" localidad="gorina"/>
+        <main className="bg-black">
+            <div className="flex h-30 w-200 pt-10 mx-300">
+                <button className=" bg-blue-200  rounded-xl w-1/4 h-10 mx-2" >Cerrar sesion</button>
+                <button className=" bg-blue-200  rounded-xl w-1/4 h-10">Ver Perfil</button>
             </div>
-            <div className="flex flex-col w-1/2 h-1/3 py-10 bg-black ">      
-            <CuadradoAlquiler className="w-1/2"nom="informatica" ciu="la plata" localidad="gorina"/>
+            <div className="ps-30 text-3xl text-white pb-8">    
+                Bienvenido a Alquileres express
+            </div>
+            <div className="ps-50 w-1/1 h-1/4 ">      
+                <CuadradoAlquiler className="w-1/2"nom=" Casa " ciu=" La Plata " localidad=" Gonnet " tipo= " Inmueble" imagen= "https://static.fotocasa.es/images/ads/24755404-8821-4632-ac79-87ca5769b5f0?rule=feed_640x480_jpg_70"/>
+                <CuadradoAlquiler className="w-1/2"nom=" Casa " ciu=" La Plata " localidad=" Gonnet " tipo= " Inmueble" imagen= "https://media.istockphoto.com/id/1470006282/es/foto/se-vende-cartel-de-bienes-ra%C3%ADces-frente-a-casa-nueva.jpg?s=612x612&w=0&k=20&c=WcZ8BP3au7Qj3HnW42qJpyM0wNbAGNkoD0WtW5Z7cos="/>     
+                <CuadradoAlquiler className="w-1/2"nom=" Casa " ciu=" La Plata " localidad=" Gonnet " tipo= " Inmueble" imagen= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO8DYnUVh3LLlTK2MfqVVwJUlEJdlVTzU0wg&s"/>
+                <CuadradoAlquiler className="w-1/2"nom=" Casa " ciu=" La Plata " localidad=" Gonnet " tipo= " Inmueble" imagen= "https://static.fotocasa.es/images/ads/24755404-8821-4632-ac79-87ca5769b5f0?rule=feed_640x480_jpg_70"/>
+                <CuadradoAlquiler className="w-1/2"nom=" Casa " ciu=" La Plata " localidad=" Gonnet " tipo= " Inmueble" imagen= "https://static.fotocasa.es/images/ads/24755404-8821-4632-ac79-87ca5769b5f0?rule=feed_640x480_jpg_70"/>
             </div>
         </main>
     </>
