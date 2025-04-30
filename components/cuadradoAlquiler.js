7489dbde6ee9f92b51236dc90b4a123d0e2b8cbf
Gasparsanchez334 <78025076+Gasparsanchez334@users.noreply.github.com>
2025-04-30 15:56:20 -0300

    Update cuadradoAlquiler.js

--- components/cuadradoAlquiler.js
+++ components/cuadradoAlquiler.js
@@ -1,22 +1,25 @@
-export default function CuadradoAlquiler ({nom, ciu, localidad}){
+export default function CuadradoAlquiler ({nom, ciu, localidad, tipo, imagen}){
     
     return <>
-        <div  className="flex bg-yellow-500 rounded-lg h-20 w-full align-center text-lg ">
-            <img src="https://static.fotocasa.es/images/ads/24755404-8821-4632-ac79-87ca5769b5f0?rule=feed_640x480_jpg_70" className="w-1/6 h=1/8 rounded-lg"></img>
+        <div  className="flex bg-yellow-600 rounded-lg h-35 w-300 text-lg ">
+            <img src= {imagen} className="w-1/6 h=1/8 rounded-lg"></img>
             <div className="grid">
                 <label className="px-2 ">
-                    nombre:{nom} 
+                    Nombre:{nom} 
                 </label>
                 <label className="px-2 ">
-                    ciudad:{ciu}
+                    Ciudad:{ciu}
                 </label>
                 <lable className="px-2 ">
-                    localidad:{localidad}
+                    Localidad:{localidad}
+                </lable>
+                <lable className="px-2 ">
+                    Tipo:{tipo}
                 </lable>
             </div>
-            <div className="flex justify-end h-12 w-150 pt-5">
-                <button className=" bg-blue-300  rounded-xl w-1/6 h-10 mx-4">Reservar </button>
-                <button className=" bg-blue-300  rounded-xl w-1/6 h-10">Ver Mas </button>
+            <div className="grid h-30 w-80 pt-7 pl-150">
+                <button className=" bg-blue-200  rounded-xl w-30 h-10 ">Reservar </button>
+                <button className=" bg-blue-200  rounded-xl w-30 h-10 ">Ver Mas </button>
             </div>
         </div>    
     </> 
