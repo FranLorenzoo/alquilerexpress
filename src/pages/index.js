2e7cdb258cc3e05df16ebd3184462ce7a453d084
AguilaSiempre22 <gonzabazan22@gmail.com>
2025-05-07 18:53:29 -0300

    correccionError

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,14 @@ export default function Pruebas (){
             <title> Alquileres</title>
         </Head>
 
-        <NavBar />
-        
-        <BarraBusqueda/>
+
  
       <main className="flex justify-center w-screen h-screen bg-blue-400">
+        
+                <NavBar />
+                
+                <BarraBusqueda/>
+
                 <div className="flex flex-col w-1/2 h-1/3 py-10 bg-blue-400">      
                     <CuadradoAlquiler nom="informatica" ciu="la plata" localidad="gorina" />
                     <CuadradoAlquiler nom="informatica" ciu="la plata" localidad="city bell" />
